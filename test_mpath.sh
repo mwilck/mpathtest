cf061e02462d273d88cfcd27972433e922af52de
Martin Wilck <mwilck@suse.com>
2017-08-23 22:24:10 +0200

    first simple multipath remove/add path test

--- test_mpath.sh
+++ test_mpath.sh
@@ -6,11 +6,63 @@ MYDIR=$(cd "$(dirname $0)" && pwd)
 . $MYDIR/cleanup_lib.sh
 
 # mpath name
-MPATH=3600601600a3020002282d7e2c5a6e411
+: ${MPATH:=3600601600a3020002282d7e2c5a6e411}
+: ${DEBUGLVL:=2}
 
-[[ -b /dev/mapper/$MPATH ]]
+msg() {
+    [[ $1 -lt $DEBUGLVL ]] && return
+    shift
+    echo "$ME: $*" >&2;
+}
 
-PATHS=($(multipathd show paths format "%m %d" | sed -n 's/3600601600a3020002282d7e2c5a6e411 //p'))
+get_path_list() {
+    multipathd show paths format "%m %d" | sed -n "s/$1 //p"
+}
 
-echo ${PATHS[@]}
+start_monitor() {
+    [[ -z "$_MONITOR_PID" && -z "$_MONITOR_CLEAN" ]]
+    udevadm monitor --env -s block >& "$TMPD"/udev-monitor.log &
+    _MONITOR_PID=$!
+    push_cleanup stop_monitor
+}
+
+stop_monitor() {
+    [[ $_MONITOR_PID ]] || return 0
+    kill $_MONITOR_PID
+    unset _MONITOR_PID
+}
+
+TMPD=$(mktemp -d /tmp/$ME-XXXXXX)
+push_cleanup rm -rf "$TMPD"
+msg 1 temp dir is $TMPD
+
+[[ -b /dev/mapper/"$MPATH" ]]
+
+PATHS=($(get_path_list "$MPATH"))
 [[ ${#PATHS[@]} -gt 0 ]]
+
+msg 2 Checking mpath $MPATH with ${#PATHS[@]} paths: ${PATHS[@]}
+
+start_monitor
+
+sleep 1
+
+for path in ${PATHS[@]}; do
+    msg 2 removing $path
+    multipathd remove path $path
+    sleep 1
+done
+
+multipathd show topology
+
+for path in ${PATHS[@]}; do
+    msg 2 adding $path
+    multipathd add path $path
+    sleep 1
+done
+
+multipathd show topology
+
+stop_monitor
+
+cat "$TMPD"/udev-monitor.log
