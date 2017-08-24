ca62f28bbe4c21e466dcaeabc7382a62ccd1bb60
Martin Wilck <mwilck@suse.com>
2017-08-24 13:48:45 +0200

    added code for creating partitions

--- test_mpath.sh
+++ test_mpath.sh
@@ -50,18 +50,60 @@ stop_monitor() {
 }
 
 delete_slaves() {
-    SLAVES=$(get_slaves $DEVNO)
-    for slv in $SLAVES; do
+    # arg $1: dm device /dev/dm-$X
+    # arg $2: dm device in major:minor format
+    local slaves slv
+    [[ $1 && $2 && -b $1 ]]
+    slaves=$(get_slaves $2)
+    for slv in $slaves; do
 	msg 2 Slave $slv will be deleted
     done
     
-    kpartx -d $DMDEV
-    SLAVES=$(get_slaves $DEVNO)
-    for slv in $SLAVES; do
+    kpartx -d $1
+    slaves=$(get_slaves $2)
+    for slv in $slaves; do
 	dmsetup remove /dev/mapper/$slv
     done
-    # Wipe partition table
-    dd if=/dev/null of=$DMDEV bs=1m count=1
+
+    msg 2 wiping partition table on $1
+    sgdisk --zap-all $DMDEV
+}
+
+create_parts_simple() {
+    # arg $1: dm device /dev/dm-$X
+    # arg $2: device size in MiB
+    # further args: partition types recognized by parted, or "lvm"
+    [[ $1 && $2 && -b $1 ]]
+    local dev=$1 sz=$2 p n i begin end type more
+    shift; shift
+    n=$#
+    {
+	cat <<EOF
+unit MiB
+mklabel gpt
+EOF
+	i=0
+	begin=1
+	end=$((sz/n))
+	for p in "$@"; do
+	    i=$((i+1))
+	    case $p in
+		lvm) type=ext2
+		     more="set $i LVM on"
+		     ;;
+		*) type=$p
+		   more=
+		   ;;
+	    esac
+	    echo mkpart part${i}_$p $type $begin $end
+	    echo $more
+	    begin=$end
+	    end=$((end+sz/n))
+	    if [[ $end -eq $sz ]]; then end=-1s; fi
+	done
+    } >$TMPD/parted.cmd
+    cat $TMPD/parted.cmd
+    parted $dev <$TMPD/parted.cmd
 }
 
 TMPD=$(mktemp -d /tmp/$ME-XXXXXX)
@@ -72,18 +114,19 @@ DMDEV=$(readlink -f /dev/mapper/$MP)
 DMNAME=${DMDEV#/dev/}
 DEVNO=$(dmsetup info -c -o major,minor --noheadings $DMDEV)
 [[ -b $DMDEV ]]
-DEVSZ_MB=$(($(blockdev --getsz /dev/dm-2)/2048))
-[[ $DEVSZ_MB -gt 1 ]]
+DEVSZ_MiB=$(($(blockdev --getsz /dev/dm-2)/2048))
+[[ $DEVSZ_MiB -gt 1 ]]
 
 PATHS=($(get_path_list "$MPATH"))
 [[ ${#PATHS[@]} -gt 0 ]]
 
 msg 2 Checking mpath $MPATH with ${#PATHS[@]} paths: ${PATHS[@]}
 
-delete_slaves
+delete_slaves $DMDEV $DEVNO
+create_parts_simple $DMDEV $DEVSZ_MiB ext2 xfs lvm
 
-start_monitor
 exit 0
+start_monitor
 
 sleep 1
 
