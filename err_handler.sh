81f0fd25ab58a92f60c61cd7ab23f83c51657b39
Martin Wilck <mwilck@suse.com>
2017-08-24 22:25:21 +0200

    save BASH_COMMAND and allow ERR_FD

--- err_handler.sh
+++ err_handler.sh
@@ -1,12 +1,15 @@
 set -e -E
-trap 'LINE=$LINENO; _err_handler' ERR
+trap 'LINE=$LINENO; BC=$BASH_COMMAND; _err_handler' ERR
 
 _err_handler() {
-    local i=1
+    local i=1 
     set +eE
     trap - ERR
+    if [[ $ERR_FD ]]; then
+	exec 2>&$ERR_FD
+    fi
     exec >&2
-    echo "$0: Error in command \"$BASH_COMMAND\" on line $LINE. Stack:"
+    echo "$0: Error in command \"$BC\" on line $LINE. Stack:"
     while [[ $i < ${#FUNCNAME[@]} ]]; do
 	printf "file %s:%s() line %d \n" \
 	       "${BASH_SOURCE[$i]}" "${FUNCNAME[$i]}" "${BASH_LINENO[((i-1))]}"
