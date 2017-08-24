246161c02e457681b73ae6c309f821f2bf50f3be
Martin Wilck <mwilck@suse.com>
2017-08-24 22:30:13 +0200

    sync to wotan, too

--- sync.sh
+++ sync.sh
@@ -1,2 +1,4 @@
 #! /bin/bash
-exec rsync -ai --exclude .git --delete --delete-excluded ./ bremer.arch.suse.de:/home/rules/
+#exec rsync -ai --exclude .git --delete --delete-excluded ./ bremer.arch.suse.de:/home/rules/
+rsync -a ./ wotan.suse.de:/suse/mwilck/rules/
+rsync -ai --exclude .git  ./ bremer.arch.suse.de:/home/rules/
